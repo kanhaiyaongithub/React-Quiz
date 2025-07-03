274ba4dc7775ab147494ee2b44e80adb838813e3
Kanhaiya Choudhary <kanhaiyaworks10@gmail.com>
2025-07-03 16:58:29 +0530

    Second Commit

---
+++ src/components/FinishedScreen.js
@@ -0,0 +1,26 @@
+function FinishedScreen({ points, maxPossiblePoints, highscore, dispatch }) {
+  const percentage = (points / maxPossiblePoints) * 100;
+  let emoji;
+  if (percentage === 100) emoji = "🎖️";
+  if (percentage === 80 && percentage < 100) emoji = "🥈";
+  if (percentage === 50 && percentage < 80) emoji = "🥉";
+  if (percentage >= 0 && percentage < 50) emoji = "⭐";
+  if (percentage === 0) emoji = "👎";
+
+  return (
+    <>
+      <p className="result">
+        <span>{emoji}</span>You scored <strong>{points}</strong> out of{" "}
+        {maxPossiblePoints}({Math.ceil(percentage)}%)
+      </p>
+      <p className="highscore">(Highscore: {highscore} points)</p>
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "restart" })}
+      >
+        Restart
+      </button>
+    </>
+  );
+}
+export default FinishedScreen;
